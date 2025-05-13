e52c7dd4db66a73943b37ae9f2c864e8ae99bf38
Murilo1602 <murilochiarello16@gmail.com>
2025-05-13 09:58:16 -0300

    Add files via upload

---
+++ main (36).c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    
+    for (int i = 1; i <= 100; i++) { //Esse for serve para que seja concluida a repetição de 1 ate 100
+        printf("%d\n", i);
+    }
+    return 0;
+}
